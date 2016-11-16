5445d58c81a9c10f935cf296857a170255cf09f5
Hal Fairless and James Lee <hal@>
2016-11-15 16:32:59 -0800

    Incomplete fails on zero remainder

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,21 +1,34 @@
 class Fixnum
   define_method(:numbers_to_words) do
     numbers_to_words = Hash.new()
-    singles = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
-    teens = { 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
-    tens = { 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety" }
-    hundreds = { 100 => "one-hundred", 200 => "two-hundred", 300 => "three-hundred", 400 => "four-hundred", 500 => "five-hundred", 600 => "six-hundred", 700 => "seven-hundred", 800 => "eight-hundred", 900 => "nine-hundred" }
-    output_number = ""
 
-    if self > 99 && self < 1000
+    singles = { 0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
+
+    teens = { 0 => "", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen" }
+
+    tens = { 0 => "", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety" }
+
+    hundreds = { 0 => "", 100 => "one-hundred", 200 => "two-hundred", 300 => "three-hundred", 400 => "four-hundred", 500 => "five-hundred", 600 => "six-hundred", 700 => "seven-hundred", 800 => "eight-hundred", 900 => "nine-hundred" }
+
+    thousands = { 0 => "", 1000 => "one-thousand", 2000 => "two-thousand", 3000 => "three-thousand", 4000 => "four-thousand", 5000 => "five-thousand", 6000 => "six-thousand", 7000 => "seven-thousand", 8000 => "eight-thousand", 9000 => "nine-thousand" }
+
+    output_number = ""
+    if self == 0
+      output_number = ("zero")
+    elsif self > 999 && self < 10000
+      teen = (self.%(100))
+      hundred = (self.%(1000))-(self.%(100))
+      thousand = self-(self.%(1000))
+      output_number = (thousands.fetch(thousand).concat(" ")).concat(hundreds.fetch(hundred).concat(" ")).concat(teens.fetch(teen))
+    elsif self > 99 && self < 1000
       single = self.%(10)
-      ten = (self.%(100))-(single)
+      ten = (self.%(100))
       hundred = self-(self.%(100))
-      output_number = (hundreds.fetch(hundred).concat(" ")).concat(tens.fetch(ten).concat(" ").concat(singles.fetch(single)))
+      output_number = (hundreds.fetch(hundred).concat(" ")).concat(teens.fetch(teen))
     elsif self > 19 && self < 100
       single = self.%(10)
       ten = self-(single)
-      output_number = (tens.fetch(ten).concat(" ").concat(singles.fetch(single)))
+      output_number = (tens.fetch(ten)).concat(" ").concat(singles.fetch(single))
     elsif self > 9 && self < 20
       output_number = teens.fetch(self)
     else
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -17,4 +17,8 @@ describe("Fixnum#numbers_to_words") do
   it("translates numbers less than one_thousand is numeric form into a written word") do
   expect(192.numbers_to_words()).to(eq("one-hundred ninety two"))
   end
+
+  it("translates numbers less than ten_thousand is numeric form into a written word") do
+  expect(7777.numbers_to_words()).to(eq("seven-thousand seven-hundred seventy seven"))
+  end
 end
